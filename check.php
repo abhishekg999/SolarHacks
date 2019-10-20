1f8a8575e38151f362d0e73dc126f7f8805d21ec
abhishekg999 <abhishekgovindarasu@gmail.com>
2019-10-19 18:06:13 -0700

    sdfasf

--- check.php
+++ check.php
@@ -16,17 +16,38 @@
 
 
 	<?php 
-		$answer = $_POST['answer'];
-		$cmd = "echo '".$answer."' > temp.py && python3 temp.py";
+		$answer = $_POST['answer'];																																																							
+
+		$cmd = "echo 'account = [0,0,0,0,0]\n".str_replace("\n", "", $answer)."\nprint(account)' > temp.py";
 	 	$result = shell_exec($cmd);
-	 	echo $result;
+	 	$cmd = "python3 temp.py";
+	 	$result = shell_exec($cmd);
 	 	$cmd = "rm temp.py";
-	 	$result = shell_exec($cmd);
+	 	$q = shell_exec($cmd);
+	 	$result = str_replace("\n", "", $result);
+	 	if($result=="[500, 500, 500, 500, 500]"){
+			echo '<div class="alert alert-dismissible alert-success">
+        	<button type="button" class="close"></button>
+        	<strong>Well done!</strong> You have solved the problem! <a href="learning.html" class="alert-link">Click here to read up about Python</a> or <a href="index.html" class="alert-link">click here to go to the home page.</a>
+			</div> <br> <h1>Output</h1><div class="jumbotron"><div class="well">'.$result.'</div></div>';
+	 	} else {
+	 		echo '<div class="alert alert-dismissible alert-danger">
+                <button type="button" class="close"></button>
+				<strong>Oh snap!</strong> <a href="#" onclick="self.close();" class="alert-link">Change a few things up</a> and try submitting again.
+				</div><br><div class="jumbotron"><div class="well">Check again! Or maybe there is a syntax error... Head over to the <a href="learning.html" class="alert-link">Additional Resources</a> for more help</div></div>';
+	 	}
+
+
 	?>
 
 
+<div>
+	
+</div>
+
 
 </div>
+
 
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-
 var speed = 20;
 var i = 0;
-var text = "This same government official has hacked into the bank accounts of 5 people and stolen \
+var text = "A corrupt government official has hacked into the bank accounts of 5 people and stolen \
 			all of their money ($500 dollars each). You have successfully disabled his program, but \
-			the fact still remains that the money is stolen. Restore the money back to their accounts.\
-			<br /> Given: An array accounts that contains the money of each person's account; account = [0, 0, 0, 0, 0];\
-			return a new array with the account values restored."
-
+			the fact still remains that the money is stolen. account = [0,0,0,0,0].\
+			Restore the money back to their accounts."
 
 
 function typeWriter() {
@@ -22,4 +19,6 @@ function typeWriter() {
 function main(){
 	typeWriter(text);
 }
+
+
 
